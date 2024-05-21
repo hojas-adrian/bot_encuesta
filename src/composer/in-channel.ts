170b6d5538690ec88af8f082af0dca3fb341c774
Adrián Hojas Moré <hojas.adrian@outlook.com>
2024-05-21 06:01:08 -0400

    add resend poll

--- src/composer/in-channel.ts
+++ src/composer/in-channel.ts
@@ -5,7 +5,7 @@ const composer = new Composer();
 
 const inChannel = composer.filter((ctx) => ctx.chat?.id === +CHANNEL_ID);
 
-inChannel.on("message", async (ctx) => {
+inChannel.on(["message", "poll"], async (ctx) => {
   const fwd = await ctx.forwardMessage(GROUP_ID);
 
   await ctx.api.pinChatMessage(GROUP_ID, fwd.message_id);

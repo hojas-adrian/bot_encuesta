bf84cf5640d239dbd0fd552e63059ba0e4484f1f
Adrián Hojas Moré <hojas.adrian@outlook.com>
2024-05-21 05:13:34 -0400

    update grammy

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
 // x/
-export * from "https://deno.land/x/grammy@v1.21.1/mod.ts";
+export * from "https://deno.land/x/grammy@v1.23.0/mod.ts";
 export * from "https://deno.land/x/grammy_transformer_throttler@v1.2.1/mod.ts";
 export * from "https://deno.land/x/grammy_runner@v2.0.3/mod.ts";

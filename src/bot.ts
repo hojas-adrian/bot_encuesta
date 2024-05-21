a73fbe0bb11106e4b33b4dc1d2838e7652f8ddb3
Adrián Hojas Moré <hojas.adrian@outlook.com>
2024-05-21 05:38:12 -0400

    update grammy and added rate limiter

--- src/bot.ts
+++ src/bot.ts
@@ -1,11 +1,10 @@
-import { apiThrottler, Bot } from "../deps.ts";
+import { Bot, limit } from "../deps.ts";
 import { BOT_TOKEN } from "./helpers/constants.ts";
 import inChannel from "./composer/in-channel.ts";
 
-export const bot = new Bot("BOT_TOKEN");
+export const bot = new Bot(BOT_TOKEN);
 
-const throttler = apiThrottler();
-bot.api.config.use(throttler);
+bot.use(limit());
 
 bot.use(inChannel);
 

157cc07cd08bb4749bf8f70b2604a13b74b30641
Adrián Hojas Moré <hojas.adrian@outlook.com>
2024-05-21 11:34:50 -0400

    refactor deps

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
 // x/
 export * from "https://deno.land/x/grammy@v1.23.0/mod.ts";
-export { limit } from "https://deno.land/x/grammy_ratelimiter@v1.2.0/mod.ts";
+export * from "https://deno.land/x/grammy_ratelimiter@v1.2.0/mod.ts";

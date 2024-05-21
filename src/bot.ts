0cf5d392738d0bbc5c72aef8c20484f6b65e0914
Adrián Hojas Moré <hojas.adrian@gmail.com>
2024-05-21 05:08:03 -0400

    test problem reading token

--- src/bot.ts
+++ src/bot.ts
@@ -2,7 +2,7 @@ import { apiThrottler, Bot } from "../deps.ts";
 import { BOT_TOKEN } from "./helpers/constants.ts";
 import inChannel from "./composer/in-channel.ts";
 
-export const bot = new Bot(BOT_TOKEN);
+export const bot = new Bot("BOT_TOKEN");
 
 const throttler = apiThrottler();
 bot.api.config.use(throttler);

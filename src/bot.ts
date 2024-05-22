88fcb5c7c632866929bb64ffe83929f93401b990
Adrián Hojas Moré <hojas.adrian@outlook.com>
2024-05-21 21:02:47 -0400

    added command ping

--- src/bot.ts
+++ src/bot.ts
@@ -1,9 +1,11 @@
 import { Bot, limit } from "../deps.ts";
 import { BOT_TOKEN } from "./helpers/constants.ts";
 import inChannel from "./composer/in-channel.ts";
+import admin from "./composer/admin.ts";
 
 export const bot = new Bot(BOT_TOKEN);
 
 bot.use(limit());
 
 bot.use(inChannel);
+bot.use(admin);
